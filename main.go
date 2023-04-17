b4dd6a3e41b3084971293c7019c8d8fe9eaab6f2
1148648777@qq.com <1148648777@qq.com>
2023-04-17 16:05:00 +0800

    refactor: use constant for port and update command line argument handling

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
 var IP string
-var PORT string = ":7684"
+
+const PORT string = ":7684"
 
 func main() {
 	ip, err := getLocalIP()
@@ -27,9 +28,11 @@ func main() {
 	// If you need to run LANWorker-web,
 	// the path to the web resources should be passed from the command line.
 	// Example:
-	//     $ LANWorker-desktop /var/www/LANWorker-web
-	if len(os.Args) >= 2 {
-		serveWebpage(os.Args[1])
+	//     $ LANWorker-desktop --path /var/www/LANWorker-web
+	webPath := flag.String("path", "", "the path of the LANWorker-web directory")
+	flag.Parse()
+	if *webPath != "" {
+		serveWebpage(*webPath)
 	}
 	startServer()
 }
